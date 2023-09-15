4d1c3aca55d2511168d59ca8ee1fbbb031ce00b0
zyf722 <1124982772@qq.com>
2023-09-15 15:33:38 +0800

    fix: fixes BD103/Flask-Rich#2,  BD103/Flask-Rich#3

--- example/app.py
+++ example/app.py
@@ -5,16 +5,26 @@ from flask_rich import RichApplication
 app = Flask(__name__)
 rich = RichApplication(app)
 
-
-app.logger.debug("If this is [red]colorful[/red], everything worked!")
-app.logger.debug("1, 2.4, True, False, None")
+app.logger.setLevel("DEBUG")
 
 
 @app.route("/")
 def index():
+    app.logger.debug("If this is [red]colorful[/red], everything worked!")
+    app.logger.debug("1, 2.4, True, False, None")
     return "INDEX"
+
+
+@app.route("/error")
+def error():
+    raise Exception
+    return "error"
 
 
 @app.route("/test/<string:x>", methods=["GET", "POST"])
 def test(x: str):
     return x
+
+
+if __name__ == "__main__":
+    app.run()
